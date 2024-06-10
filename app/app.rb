0aea4d7cf504539fa8756713ae429538d174b67a
lucascheistwer <lucas.cheistwer@zerf.tech>
2024-06-09 22:31:08 -0300

    Add static files

--- app/app.rb
+++ app/app.rb
@@ -6,5 +6,15 @@ class MainApplication < Sinatra::Base
   use AuthController
   use ProductsController
 
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
+  before '/authors' do
+    expires 86_400, :public, :must_revalidate
+  end
+
+  before '/openapi.yaml' do
+    cache_control :no_cache
+  end
+
   run! if app_file == $0
 end
